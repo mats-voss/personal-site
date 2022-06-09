33552b017d8ade5b9e19402a5b23668a7a25468d
Mats Voss <74510757+mats-voss@users.noreply.github.com>
2022-06-09 21:14:13 +0200

    implemented react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/home";
+import AboutPage from "./pages/about";
+import ProjectsPage from "./pages/projects";
+import ResumePage from "./pages/resume";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Home />
-    </React.StrictMode>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/resume" element={<ResumePage />} />
+        </Routes>
+    </Router>
 );
