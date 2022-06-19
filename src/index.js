4a6d10b29b793ef5cd4ed145bae8e34ad4d44c64
Mats Voss <74510757+mats-voss@users.noreply.github.com>
2022-06-19 17:48:04 +0200

    added timeline

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import "./index.css";
 import Home from "./pages/home";
 import AboutPage from "./pages/about";
 import ProjectsPage from "./pages/projects";
-import ResumePage from "./pages/resume";
+import TimeLinePage from "./pages/timeline";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Router>
+    <Router basename="/">
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/resume" element={<ResumePage />} />
+            <Route path="/timeline" element={<TimeLinePage />} />
         </Routes>
     </Router>
 );
