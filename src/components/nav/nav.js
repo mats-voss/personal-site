e1b0f212d4bbfa4fdad54754c585e9350eeea13f
Mats Voss <74510757+mats-voss@users.noreply.github.com>
2022-06-19 17:46:42 +0200

    rebuild responsiv nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { Spin as Hamburger } from "hamburger-react";
 
 import { FaGithub } from "react-icons/fa";
 import { BsMailbox2 } from "react-icons/bs";
@@ -7,57 +8,81 @@ import { AiFillLinkedin } from "react-icons/ai";
 
 import "./nav.css";
 
-const Nav = ({ siteTitle }) => {
-    const openLink = (url) => {
-        window.open(url, "_blank", "noreferrer");
+const Nav = ({ siteTitle, color }) => {
+    const navRef = useRef();
+
+    const toggleNav = () => {
+        navRef.current.classList.toggle("toggle");
+        setNavOpen(!navOpen);
     };
+
+    const [navOpen, setNavOpen] = useState(false);
     return (
-        <nav className="navbar">
-            {/* <img src={require('../images/DevLogo001.png')} alt="Logo" /> */}
-            <Link to="/">
-                <p className="hover-effect">{siteTitle}</p>
-            </Link>
-            <ul className="text__links">
-                <li className="hover-effect">
-                    <Link to="/about">
-                        <i class="twa twa-woman-technologist-medium-light-skin-tone"></i>{" "}
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/projects" className="hover-effect">
-                        <i className="twa twa-card-file-box"></i> Projects
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/resume" className="hover-effect">
-                        <i className="twa twa-bookmark-tabs"></i> Resume
-                    </Link>
-                </li>
-            </ul>
-            <ul className="social__links">
-                <li className="hover-effect">
-                    <FaGithub
-                        className="icon"
-                        onClick={() => openLink("http://github.mats-voss.de")}
-                    />
-                </li>
-                <li className="hover-effect">
-                    <BsMailbox2
-                        className="icon"
-                        onClick={() =>
-                            (window.location = "mailto:privat@mats-voss.de")
-                        }
-                    />
-                </li>
-                <li className="hover-effect">
-                    <AiFillLinkedin
-                        className="icon"
-                        onClick={() => openLink("http://linkedin.mats-voss.de")}
-                    />
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className="navbar toggle" ref={navRef}>
+                {/* <img src={require('../images/DevLogo001.png')} alt="Logo" /> */}
+                <Link to="/" onClick={toggleNav}>
+                    <h1 className="hover-effect">{siteTitle}</h1>
+                </Link>
+                <ul className="text__links">
+                    <li className="hover-effect" onClick={toggleNav}>
+                        <Link to="/about">
+                            <i className="twa twa-woman-technologist-medium-light-skin-tone"></i>{" "}
+                            About
+                        </Link>
+                    </li>
+                    <li onClick={toggleNav}>
+                        <Link to="/projects" className="hover-effect">
+                            <i className="twa twa-card-file-box"></i> Projects
+                        </Link>
+                    </li>
+                    <li onClick={toggleNav}>
+                        <Link to="/timeline" className="hover-effect">
+                            <i className="twa twa-bookmark-tabs"></i> Timeline
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="social__links">
+                    <li className="hover-effect" onClick={toggleNav}>
+                        <a
+                            href="https://github.com/mats-voss"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaGithub />
+                        </a>
+                    </li>
+                    <li className="hover-effect" onClick={toggleNav}>
+                        <a
+                            href="https://github.com/mats-voss"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <BsMailbox2 />
+                        </a>
+                    </li>
+                    <li className="hover-effect" onClick={toggleNav}>
+                        <a
+                            href="https://github.com/mats-voss"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <AiFillLinkedin />
+                        </a>
+                    </li>
+                    {/* <ThemeToggle /> */}
+                </ul>
+            </nav>
+            <div className="styledHamburger" onClick={toggleNav}>
+                <Hamburger
+                    toggled={navOpen}
+                    toggle={setNavOpen}
+                    rounded
+                    label="Show menu"
+                    color={navOpen ? "#fff" : color ? color : "#fff"}
+                ></Hamburger>
+            </div>
+        </>
     );
 };
 
